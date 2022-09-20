c5e2e0784f5cac2aadc32d41990340ab9c3362b2
haseeb129ciit <haseeb129ciit@gmail.com>
2022-09-20 12:41:35 +0500

    Add to home screen feature added

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import "./App.css";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link, Route, BrowserRouter, Switch } from "react-router-dom";
+// import firebase from "./services/firbaseService";
+//COMPONETS
+import About from "./components/about";
+import Home from "./components/home";
+import Users from "./components/users";
+
+function App() {
+  // useEffect(() => {
+  //   const message = firebase.messaging();
+  //   message.requestPermission().then(() => {
+  //     return message.getToken()
+  //   }).then((data) => {
+  //     console.warn("TOKEN:", data)
+  //   })
+  // }, []);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/users">Users</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/users" component={Users}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/firbaseService.js
@@ -0,0 +1,13 @@
+
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ_Eyb-AQfDzVnlEsQQ3O_4DI_QaZNYz8",
+    authDomain: "pwa-push-noti-app.firebaseapp.com",
+    projectId: "pwa-push-noti-app",
+    storageBucket: "pwa-push-noti-app.appspot.com",
+    messagingSenderId: "823890112110",
+    appId: "1:823890112110:web:5f669c9c6a9c9c13ffc07f",
+    measurementId: "G-TBMEELWKF4"
+};
+firebase.initializeApp(firebaseConfig)
+export default firebase
