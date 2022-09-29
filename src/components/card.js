baa5a72cefe5c627d7c8f417f503098b2f3b7378
haseeb129ciit <haseeb129ciit@gmail.com>
2022-09-29 16:08:20 +0500

    remove link from card

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,21 @@
-
-
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
 const CardComp = ({ user }) => {
-    // console.log(user)
-    return (
+  // console.log(user)
+  return (
+    <Card style={{ margin: "auto" }}>
+      <Card.Body>
+        <Card.Title>{user.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          @{user.username}
+        </Card.Subtitle>
+        <Card.Text>
+          PHONE : {user.phone} <br />
+          EMAIL : {user.email}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
-        < Card style={{ margin: 'auto' }
-        }>
-            <Card.Body>
-                <Card.Title>{user.name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">@{user.username}</Card.Subtitle>
-                <Card.Text>
-                    PHONE : {user.phone} <br />
-                   EMAIL : {user.email}
-                </Card.Text>
-                <Card.Link href={`${user.website}`}>Website</Card.Link>
-
-            </Card.Body>
-        </Card >
-    )
-}
-
-export default CardComp
+export default CardComp;
